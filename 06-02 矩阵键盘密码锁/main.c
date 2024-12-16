0b8808a7e695ff5fdb0b111d9c9e35819de29b3e
SlenderData <mayunji2001@outlook.com>
2024-12-17 06:15:54 +0800

    Add files

--- 06-02 矩阵键盘密码锁/main.c
+++ 06-02 矩阵键盘密码锁/main.c
@@ -6,7 +6,7 @@
 unsigned char KeyNum;  // 当前矩阵键盘按键
 unsigned char Password[16];  // 与课程视频不同的数组实现，不受密码位数限制
 unsigned char *PasswordPtr = Password;  // 指向当前输入的密码位的指针
-unsigned char CorrectPassword[] = {1, 1, 4, 5, 1, 4};  // 正确密码
+unsigned char CorrectPassword[] = "1145141919810";  // 正确密码
 unsigned char i;  // 用于循环
 
 // 密码输入，成功返回 1，失败返回 0
@@ -15,7 +15,7 @@ unsigned char AddPassword(unsigned char InputNum)
 {
 	if(InputNum > 10) return 0;  // 大于 10 时均为非数字输入
 	if(PasswordPtr >= Password + 16) return 0;  // 超出LCD显示范围
-	*PasswordPtr = InputNum;
+	*PasswordPtr = (InputNum == 10) ? 0 : InputNum;  // 按键 10 代表 0
 	PasswordPtr++;
 	LCD_ShowString(2, 1, "                ");  // 清空密码显示
 	for(i = 0; i < PasswordPtr - Password; i++)  // 显示密码
@@ -43,15 +43,15 @@ unsigned char Backspace(unsigned char InputKey)
 unsigned char CheckPassword(unsigned char InputKey)
 {
 	if(InputKey != 11) return 0;  // 非确认键
-	if((PasswordPtr - Password) != sizeof(CorrectPassword))  // 密码位数不符
+	if((PasswordPtr - Password) != (sizeof(CorrectPassword) - 1))  // 密码位数不符
 	{
 		LCD_ShowString(2, 1, "                ");  // 清空密码显示
 		LCD_ShowString(2, 1, "Wrong!");  // 显示密码错误提示
 		return 1;
 	}
-	for(i = 0; i < sizeof(CorrectPassword); i++)
+	for(i = 0; i < sizeof(CorrectPassword) - 1; i++)
 	{
-		if(Password[i] != CorrectPassword[i])  // 密码错误
+		if(Password[i] != (CorrectPassword[i] - '0'))  // 密码错误
 		{
 			LCD_ShowString(2, 1, "                ");  // 清空密码显示
 			LCD_ShowString(2, 1, "Wrong!");  // 显示密码错误提示
